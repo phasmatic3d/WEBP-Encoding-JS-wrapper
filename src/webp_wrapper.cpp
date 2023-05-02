a7bc5a91b320f33366de427e3c8bbf68d71e42cf
Vineek <nickvitsas@gmail.com>
2023-05-02 10:04:22 +0300

    Updated API

--- src/webp_wrapper.cpp
+++ src/webp_wrapper.cpp
@@ -14,53 +14,73 @@ extern "C" int wpGetEncoderVersion() {
   return WebPGetEncoderVersion();
 }
 
-extern "C" int wpGetWidth(const uint8_t* encoded_data, unsigned int encoded_data_size) {
+int getWidth(const emscripten::val& data) {
+  std::vector<uint8_t> bytes{};
+  bytes.resize(data["byteLength"].as<size_t>());
+
+  emscripten::val memory = emscripten::val::module_property("HEAP8")["buffer"];
+  emscripten::val memoryView = data["constructor"].new_(memory, reinterpret_cast<uintptr_t>(bytes.data()), data["length"].as<uint32_t>());
+  memoryView.call<void>("set", data);
   int width = -1;
   int height = -1;
-  int ok = WebPGetInfo(encoded_data, (size_t) encoded_data_size, &width, &height);
-  if (!ok) {
-    return -1;
-  }
-  return width;
+  int ok = WebPGetInfo(bytes.data(), bytes.size(), &width, &height);
+  return (ok) ? width : -1;
 }
 
-extern "C" int wpGetHeight(const uint8_t* encoded_data, unsigned int encoded_data_size) {
+int getHeight(const emscripten::val& data) {
+  std::vector<uint8_t> bytes{};
+  bytes.resize(data["byteLength"].as<size_t>());
+
+  emscripten::val memory = emscripten::val::module_property("HEAP8")["buffer"];
+  emscripten::val memoryView = data["constructor"].new_(memory, reinterpret_cast<uintptr_t>(bytes.data()), data["length"].as<uint32_t>());
+  memoryView.call<void>("set", data);
   int width = -1;
   int height = -1;
-  int ok = WebPGetInfo(encoded_data, (size_t) encoded_data_size, &width, &height);
-  if (!ok) {
-    return -1;
-  }
-  return height;
+  int ok = WebPGetInfo(bytes.data(), bytes.size(), &width, &height);
+  return (ok) ? height : -1;
 }
 
-extern "C" uint8_t* wpEncode(const uint8_t* raw_data, int width, int height, int comps, float quality, int* encoded_data_size) {
-  uint8_t* encodeded_buffer = NULL; 
-  size_t raw_data_size = 0;
+emscripten::val encode(const emscripten::val& data, int width, int height, int comps, float quality) {
+  std::vector<uint8_t> bytes{};
+  bytes.resize(data["byteLength"].as<size_t>());
+
+  emscripten::val memory = emscripten::val::module_property("HEAP8")["buffer"];
+  emscripten::val memoryView = data["constructor"].new_(memory, reinterpret_cast<uintptr_t>(bytes.data()), data["length"].as<uint32_t>());
+  memoryView.call<void>("set", data);
+
+  uint8_t* encoded_data = nullptr; 
+  size_t encoded_data_size = 0;
   if(comps == 3)
-    raw_data_size = WebPEncodeRGB((const uint8_t*) raw_data, width, height, width * comps, quality, &encodeded_buffer);
+    encoded_data_size = WebPEncodeRGB((const uint8_t*) bytes.data(), width, height, width * comps, quality, &encoded_data);
   else if (comps == 4)
-    raw_data_size = WebPEncodeRGBA((const uint8_t*) raw_data, width, height, width * comps, quality, &encodeded_buffer);
+    encoded_data_size = WebPEncodeRGBA((const uint8_t*) bytes.data(), width, height, width * comps, quality, &encoded_data);
   else
-    return encodeded_buffer;
+    return emscripten::val::array();
 
-  *encoded_data_size = (int)raw_data_size;
-
-  return encodeded_buffer;
+  return emscripten::val(
+            emscripten::typed_memory_view(encoded_data_size,
+                                          encoded_data));      
 } 
 
-extern "C" uint8_t* wpRescale(const uint8_t* raw_data, int width, int height, int comps, int scaled_width, int scaled_height) {
+emscripten::val rescale(const emscripten::val& data, int width, int height, int comps, int scaled_width, int scaled_height) {
+  std::vector<uint8_t> bytes{};
+  bytes.resize(data["byteLength"].as<size_t>());
+
+  emscripten::val memory = emscripten::val::module_property("HEAP8")["buffer"];
+  emscripten::val memoryView = data["constructor"].new_(memory, reinterpret_cast<uintptr_t>(bytes.data()), data["length"].as<uint32_t>());
+  memoryView.call<void>("set", data);
+
   uint8_t* raw_data_scaled = nullptr;
   int result = -1;
   WebPPicture pic;
-  if (!WebPPictureInit(&pic)) return raw_data_scaled;  // version error
+  if (!WebPPictureInit(&pic)) return emscripten::val::array();  // version error
   pic.width = width;
   pic.height = height;
   pic.use_argb = 1;
-  if (!WebPPictureAlloc(&pic)) return raw_data_scaled;   // memory error
-  WebPPictureImportRGBA(&pic, raw_data, pic.argb_stride * comps);
+  if (!WebPPictureAlloc(&pic)) return emscripten::val::array();   // memory error
+  WebPPictureImportRGBA(&pic, bytes.data(), pic.argb_stride * comps);
   result = WebPPictureRescale(&pic, scaled_width, scaled_height);
-  if (!result) return raw_data_scaled;
+  if (!result) return emscripten::val::array();
   
   raw_data_scaled = (uint8_t*)malloc(scaled_width * scaled_height * comps);
   const uint8_t* argb = (const uint8_t*)pic.argb;
@@ -75,10 +95,19 @@ extern "C" uint8_t* wpRescale(const uint8_t* raw_data, int width, int height, in
   }
 
   WebPPictureFree(&pic);
-  return raw_data_scaled;
+  return emscripten::val(
+            emscripten::typed_memory_view(scaled_width * scaled_height * comps,
+                                          raw_data_scaled));      
 } 
 
-extern "C" int wpDecode(const uint8_t* encoded_data, unsigned int encoded_data_size, unsigned char* decoded_data) {
+emscripten::val decode(const emscripten::val& data) {
+  std::vector<uint8_t> bytes{};
+  bytes.resize(data["byteLength"].as<size_t>());
+
+  emscripten::val memory = emscripten::val::module_property("HEAP8")["buffer"];
+  emscripten::val memoryView = data["constructor"].new_(memory, reinterpret_cast<uintptr_t>(bytes.data()), data["length"].as<uint32_t>());
+  memoryView.call<void>("set", data);
+
   WebPBitstreamFeatures features = {0};
   VP8StatusCode status = VP8_STATUS_OK;
   int decoded_data_size;
@@ -86,17 +115,102 @@ extern "C" int wpDecode(const uint8_t* encoded_data, unsigned int encoded_data_s
   int width = -1;
   int height = -1;
   
-  ok = WebPGetInfo(encoded_data, (size_t) encoded_data_size, &width, &height);
+  ok = WebPGetInfo(bytes.data(), bytes.size(), &width, &height);
   if (!ok) {
-    return -1;
+    return emscripten::val::array();
   }
 
-  status = WebPGetFeatures(encoded_data, (size_t) encoded_data_size, &features);
+  status = WebPGetFeatures(bytes.data(), bytes.size(), &features);
 
   decoded_data_size = width * height * 4;
+  uint8_t* decoded_data = (uint8_t*)malloc(decoded_data_size);
   const uint8_t* ret = WebPDecodeRGBAInto(
-      (const uint8_t*) encoded_data, encoded_data_size,
+      (const uint8_t*) bytes.data(), bytes.size(),
       decoded_data, decoded_data_size, width * 4);
+  return emscripten::val(
+            emscripten::typed_memory_view(decoded_data_size,
+                                          decoded_data));
+}
 
-  return width;
+namespace webp
+{
+  class texture
+    {
+      public:
+        texture(texture&) = delete;
+        texture(texture&& other) = default;
+        
+        static texture createFromMemory(const emscripten::val& data)
+        {
+            std::vector<uint8_t> bytes{};
+            bytes.resize(data["byteLength"].as<size_t>());
+            // Yes, this code IS copying the data. Sigh! According to Alon
+            // Zakai:
+            //     "There isn't a way to let compiled code access a new
+            //     ArrayBuffer. The compiled code has hardcoded access to the
+            //     wasm Memory it was instantiated with - all the pointers it
+            //     can understand are indexes into that Memory. It can't refer
+            //     to anything else, I'm afraid."
+            //
+            //     "In the future using different address spaces or techniques
+            //     with reference types may open up some possibilities here."
+            emscripten::val memory = emscripten::val::module_property("HEAP8")["buffer"];
+            emscripten::val memoryView = data["constructor"].new_(memory, reinterpret_cast<uintptr_t>(bytes.data()), data["length"].as<uint32_t>());
+            memoryView.call<void>("set", data);
+
+             WebPBitstreamFeatures features = {0};
+            VP8StatusCode status = VP8_STATUS_OK;
+            int decoded_data_size;
+            int ok = 0;
+            int width = -1;
+            int height = -1;
+            
+            ok = WebPGetInfo(bytes.data(), bytes.size(), &width, &height);
+            if (!ok) {
+              return texture();
+            }
+
+            status = WebPGetFeatures(bytes.data(), bytes.size(), &features);
+
+            decoded_data_size = width * height * 4;
+            uint8_t* decoded_data = (uint8_t*)malloc(decoded_data_size);
+            const uint8_t* ret = WebPDecodeRGBAInto(
+                (const uint8_t*) bytes.data(), bytes.size(),
+                decoded_data, decoded_data_size, width * 4);
+
+            return texture(decoded_data, width, height, 4);
+        }
+
+        uint32_t getWidth() const { return m_width; }
+        uint32_t getHeight() const { return m_height; }
+        uint32_t getComponentCount() const { return m_comps; }
+        emscripten::val getData() const { 
+          return emscripten::val(
+            emscripten::typed_memory_view(m_width * m_height * m_comps,
+                                          m_data));
+        }
+      private:
+        texture() : texture(nullptr, -1, -1, 0) {}
+        texture(uint8_t* data, uint32_t width, uint32_t height, uint32_t comps)
+            : m_data { data }, m_width{ width }, m_height{ height }, m_comps { comps } { }
+        uint32_t m_width;
+        uint32_t m_height;
+        uint32_t m_comps;
+        uint8_t* m_data;
+    };
+
+}
+EMSCRIPTEN_BINDINGS(WebpLibrary) {
+  class_<webp::texture>("webpTexture")
+        .constructor(&webp::texture::createFromMemory)
+        .property("data", &webp::texture::getData)
+        .property("width", &webp::texture::getWidth)
+        .property("height", &webp::texture::getHeight)
+        .property("comps", &webp::texture::getComponentCount)
+    ;
+
+    function("encode", &encode);
+    function("rescale", &rescale);
+    function("getWidth", &getWidth);
+    function("getHeight", &getHeight);
 }
